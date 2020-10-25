3dd8862532f139b02f64b71c6510f9ceb39df5b0
farid <farid@example.com>
2020-10-25 12:22:34 +0600

    update, delete

--- src/main/java/com/exam/User.java
+++ src/main/java/com/exam/User.java
@@ -10,17 +10,17 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
+import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 
 @ManagedBean(name = "user", eager = true)
-@ViewScoped
+@RequestScoped
 public class User {
 
     private int id;
@@ -31,8 +31,15 @@ public class User {
     private String[] skill;
     private String address;
     private Connection conn = null;
+    private boolean isEdit = false;
+    List<User> userList = new ArrayList<User>();
 
-    public void store() {
+    @PostConstruct
+    public void init() {
+        getShowUserList();
+    }
+
+    public String store() {
         FacesContext context = FacesContext.getCurrentInstance();
         try {
             conn = DBAccess.getConnection();
@@ -44,13 +51,13 @@ public class User {
             pst.setString(3, mobile);
             pst.setString(4, course);
             pst.setString(5, gender);
-            pst.setString(6, Arrays.toString(skill));
+            pst.setString(6, String.join(",", skill));
             pst.setString(7, address);
             int rs = pst.executeUpdate();
 
             if (rs > 0) {
                 context.addMessage("Success", new FacesMessage("Save successful"));
-
+                userList.add(this);
             } else {
                 context.addMessage("Error", new FacesMessage("Save failed!"));
 
@@ -58,12 +65,42 @@ public class User {
         } catch (SQLException ex) {
             Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
         }
-
+        return "index.xhtml";
     }
 
-    public List<User> getShowUserList() {
+    public String update() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        try {
+            conn = DBAccess.getConnection();
+            String sql = "update users set name=?, mobile=?, course=?, gender=?, skill=?, address=? "
+                    + "where id=?";
+            PreparedStatement pst = conn.prepareStatement(sql);
+
+            pst.setString(1, name);
+            pst.setString(2, mobile);
+            pst.setString(3, course);
+            pst.setString(4, gender);
+            pst.setString(5, String.join(",", skill));
+            pst.setString(6, address);
+            pst.setInt(7, id);
+            int rs = pst.executeUpdate();
+
+            if (rs > 0) {
+                context.addMessage("Success", new FacesMessage("Update successful"));
+                //userList.add(this);
+            } else {
+                context.addMessage("Error", new FacesMessage("Update failed!"));
+
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return "index.xhtml";
+    }
+
+    public void getShowUserList() {
         conn = DBAccess.getConnection();
-        List<User> userList = new ArrayList<>();
+
         try {
             //statment
             String sql = "select * from users;";
@@ -77,7 +114,7 @@ public class User {
                 String mobile = rs.getString("mobile");
                 String course = rs.getString("course");
                 String gender = rs.getString("gender");
-                //String skill = rs.getString("skill");
+                String skill = rs.getString("skill");
                 String address = rs.getString("address");
 
                 user.setId(id);
@@ -85,13 +122,68 @@ public class User {
                 user.setMobile(mobile);
                 user.setCourse(course);
                 user.setGender(gender);
+                user.setSkill(skill.split(","));
                 user.setAddress(address);
-                
+
                 userList.add(user);
             }
         } catch (Exception e) {
         }
-        return userList;
+    }
+
+    public String edit(int id) {
+        conn = DBAccess.getConnection();
+
+        try {
+            //statment
+            String sql = "select * from users where id=?";
+            PreparedStatement pst = conn.prepareStatement(sql);
+            pst.setInt(1, id);
+            ResultSet rs = pst.executeQuery();
+
+            if (rs.next()) {
+
+                String name = rs.getString("name");
+                String mobile = rs.getString("mobile");
+                String course = rs.getString("course");
+                String gender = rs.getString("gender");
+                //String skill = rs.getString("skill");
+                String address = rs.getString("address");
+
+                setId(id);
+                setName(name);
+                setMobile(mobile);
+                setCourse(course);
+                setGender(gender);
+                setAddress(address);
+
+                setIsEdit(true);
+            }
+        } catch (Exception e) {
+        }
+
+        return "/index.xhtml";
+    }
+
+    public String delete(int id) {
+        conn = DBAccess.getConnection();
+        FacesContext context = FacesContext.getCurrentInstance();
+        try {
+            //statment
+            String sql = "delete from users where id=?";
+            PreparedStatement pst = conn.prepareStatement(sql);
+            pst.setInt(1, id);
+            int rs = pst.executeUpdate();
+
+            if (rs > 0) {
+                context.addMessage("success", new FacesMessage("Delete successful"));
+            }else{
+                context.addMessage("Failed!", new FacesMessage("Delete failed!"));
+            }
+        } catch (Exception e) {
+        }
+
+        return "index.xhtml";
     }
 
     public int getId() {
@@ -148,6 +240,22 @@ public class User {
 
     public void setAddress(String address) {
         this.address = address;
+    }
+
+    public boolean isIsEdit() {
+        return isEdit;
+    }
+
+    public void setIsEdit(boolean isEdit) {
+        this.isEdit = isEdit;
+    }
+
+    public List<User> getUserList() {
+        return userList;
+    }
+
+    public void setUserList(List<User> userList) {
+        this.userList = userList;
     }
 
 }
